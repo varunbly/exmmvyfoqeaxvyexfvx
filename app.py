eb48f1ae05509815e485c23b247ffde5dc9abc5e
Varun Agarwal <varun14.5.2005@gmail.com>
2025-09-06 12:06:58 +0530

    Initial flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Flask world"
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    data = request.json
+    name = data.get("name", "Guest")
+    return jsonify({"message": f"Hello, {name}!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)

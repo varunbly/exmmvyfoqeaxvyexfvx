a27bf6e36b3533af6ac245d220f3a25d209055cf
Varun Agarwal <varun14.5.2005@gmail.com>
2025-09-06 17:40:27 +0530

    initial html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Varun is great"
+    return render_template('index.html')
 
 @app.route("/greet", methods=["POST"])
 def greet():
